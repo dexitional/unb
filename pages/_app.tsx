ee213808c0f844de051b9bbb77bd01ea39f69ccc
dexitional <ebenezerkb.ackah@gmail.com>
2023-01-20 15:39:45 +0000

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import OneSignal from 'react-onesignal';
+//import OneSignal from 'react-onesignal';
 import Analytics from 'analytics'
 //@ts-ignore
 import googleAnalytics from '@analytics/google-analytics'
+import { useEffect } from 'react';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   // One-Signal Integration
-  OneSignal.init({ appId: '2403f9ce-30d0-4d3e-95e3-748cb571bce3' });
+ // OneSignal.init({ appId: '2403f9ce-30d0-4d3e-95e3-748cb571bce3' });
   // Google Analytics Integration
   const analytics = Analytics({
     app: 'uccnoticeboaed.info',
@@ -20,6 +21,25 @@ function MyApp({ Component, pageProps }: AppProps) {
   })
   /* Track a page view */
   analytics.page()
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      // @ts-ignore
+      window.OneSignal = window.OneSignal || [];
+      OneSignal.push(function () {
+        OneSignal.init({
+          appId: "2403f9ce-30d0-4d3e-95e3-748cb571bce3",
+          notifyButton: {
+            enable: true,
+          },
+        });
+      });
+    }
+    return () => {
+      // @ts-ignore
+      window.OneSignal = undefined;
+    };
+  }, []);
 
   return <Component {...pageProps} />
 }
