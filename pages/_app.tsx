2907f541e4d72762e130ae0a769f2490e1a46368
dexitional <ebenezerkb.ackah@gmail.com>
2023-01-20 14:17:17 +0000

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import OneSignal from 'react-onesignal';
 import Analytics from 'analytics'
 //@ts-ignore
 import googleAnalytics from '@analytics/google-analytics'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
+  // One-Signal Integration
+  OneSignal.init({ appId: '2403f9ce-30d0-4d3e-95e3-748cb571bce3' });
+  // Google Analytics Integration
   const analytics = Analytics({
     app: 'uccnoticeboaed.info',
     plugins: [
@@ -15,10 +18,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       })
     ]
   })
-
   /* Track a page view */
   analytics.page()
-  
+
   return <Component {...pageProps} />
 }
 
