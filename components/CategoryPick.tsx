15cea4ab730fd7342b91fc5083d5dda8e53baea9
dexitional <ebenezerkb.ackah@gmail.com>
2023-01-24 12:19:29 +0000

    nice

--- components/CategoryPick.tsx
+++ components/CategoryPick.tsx
@@ -8,7 +8,7 @@ function CategoryPick({ data }: any) {
         <h2 className="my-5 text-xl sm:text-2xl text-white font-semibold font-epilogue">Category Topics</h2>
         <div className="w-full flex flex-wrap gap-2 sm:gap-3 text-xs sm:text-[0.97rem] font-sans font-semibold text-gray-700">
           { data.map((row: any, i: number) => {
-              const color = colors[(i%colors.length)]
+              const color = colors[(i%(colors.length-1))]
               return <Link key={i} href={`/tag/${row.slug.current}`} className={`p-2 rounded-md bg-[${color}] font-jetbrains`}>{row.title}</Link>
           })}
         </div>
