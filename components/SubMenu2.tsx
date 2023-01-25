1f06219859878558b7005e7489ec64e50e861310
dexitional <ebenezerkb.ackah@gmail.com>
2023-01-25 12:39:10 +0000

    nice

--- components/SubMenu2.tsx
+++ components/SubMenu2.tsx
@@ -3,7 +3,7 @@ import SubMenuRightPill from './SubMenuRightPill'
 
 function SubMenu2({ data }: any) {
   return (
-    <div className="py-5 px-5 w-52 flex flex-col space-y-2 bg-white border shadow-xl rounded-b-lg absolute top-[3.6rem]">
+    <div className="py-5 px-5 w-64 flex flex-col space-y-2 bg-white border shadow-xl rounded-b-lg absolute top-[3.6rem]">
       { data?.map((row: any,i:React.Key) => (
         <SubMenuRightPill title={row.title} link={`/${row.slug.current}`} />
       ))}
