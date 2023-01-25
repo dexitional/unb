5856b941b1f5cade611f0d24ddeef6cfe5fa70b0
dexitional <ebenezerkb.ackah@gmail.com>
2023-01-25 13:10:01 +0000

    nice

--- components/SubMenu2.tsx
+++ components/SubMenu2.tsx
@@ -5,7 +5,7 @@ function SubMenu2({ data }: any) {
   return (
     <div className="py-5 px-5 w-64 flex flex-col space-y-2 bg-white border shadow-xl rounded-b-lg absolute top-[3.6rem]">
       { data?.map((row: any,i:React.Key) => (
-        <SubMenuRightPill title={row.title} link={`/${row.slug.current}`} />
+        <SubMenuRightPill title={row.title} link={`/tag/${row.slug.current}`} />
       ))}
     </div>
   )
