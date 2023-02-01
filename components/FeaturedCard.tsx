c8c2a73209640781d79c14fe61e5269791230236
dexitional <ebenezerkb.ackah@gmail.com>
2023-02-01 16:23:23 +0000

    color fix

--- components/FeaturedCard.tsx
+++ components/FeaturedCard.tsx
@@ -8,17 +8,17 @@ import { urlFor } from '../sanity'
 function FeaturedCard({ data }: any) {
   
   return (
-    <div className="w-full sm:w-[57.5%] transition-all duration-200 shadow-xl shadow-blue-600/10 hover:shadow-blue-600/20 flex space-x-4 rounded-xl sm:rounded-3xl overflow-x-scroll scrollbar-hide snap-x snap-mandatory scroll-ml-4">
+    <div className="w-full md:w-[57.5%] transition-all duration-200 shadow-xl shadow-blue-600/10 hover:shadow-blue-600/20 flex space-x-4 rounded-xl sm:rounded-3xl overflow-x-scroll scrollbar-hide snap-x snap-mandatory scroll-ml-4">
       { data?.map((row: any,i: React.Key) => (
       <Link key={i} href={`/${row.slug.current}`} className="w-full flex-shrink-0 snap-center">
       <div className="overflow-hidden bg-red-100">
-          <div className="sm:h-[25rem] bg-blue-900">
-            <Image src={row.mainImage ? urlFor(row.mainImage).url(): Logo} alt="Featured Image" height={100} width={600} style={{ height:'400px',width:'100%', objectFit:'cover' }} priority />
+          <div className="relative h-48 md:h-[25rem] bg-blue-900">
+            <Image src={row.mainImage ? urlFor(row.mainImage).url(): Logo} alt="Featured Image" height={100} width={600} className="w-full h-48 md:h-[25rem] object-cover" priority />
           </div>
-          <div className="px-3 sm:px-8 py-5 sm:py-6 bg-slate-50 flex flex-col space-y-3">
-          <h1 className="text-lg sm:text-3xl font-semibold font-epilogue text-gray-800">{row.title}</h1>
+          <div className="px-3 md:px-8 py-5 sm:py-6 bg-slate-50 flex flex-col space-y-3">
+          <h1 className="text-md md:text-3xl font-medium md:font-semibold leading-tight md:leading-normal font-epilogue text-gray-800">{row.title}</h1>
           <div className="flex items-center space-x-1 sm:space-x-3">
-              <div className="h-8 w-8 sm:h-12 sm:w-12 bg-blue-800 border-2 border-yellow-500 rounded-full flex items-center justify-center font-bold text-2xl text-white">G</div>
+              <div className="h-8 w-8 md:h-12 md:w-12 bg-blue-800 border-2 border-yellow-500 rounded-full flex items-center justify-center font-bold text-2xl text-white">G</div>
               <span className="text-xs sm:text-base text-blue-600">{row.name}</span>
           </div>
           <span className="text-xs sm:text-sm text-gray-600">{ moment(row._createdAt).format('LL') }</span>
