d0e295b7946e599a134a3fc47873cd20df4a9b2d
dexitional <ebenezerkb.ackah@gmail.com>
2023-02-20 15:14:49 +0000

    new ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,9 @@ function MyDocument() {
          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600;700;800&display=swap" rel="stylesheet" />
          <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;400;500;600;700;800&display=swap" rel="stylesheet" />
          <Script async src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"></Script>
-         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4676341155028169"></Script>
+         {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4676341155028169"></Script> */}
+         {/* @ts-ignore */}
+         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4676341155028169" crossorigin="anonymous"></script>
       </Head>
      
       <body className="font-inter">
